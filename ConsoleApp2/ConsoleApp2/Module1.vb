f1aff0e28d78a0b55da83abd07902943a8a4827b
TeoKhaiLing <36321600+TeoKhaiLing@users.noreply.github.com>
2019-02-05 12:16:57 +0800

    First Adding Files
    First Adding

---
+++ ConsoleApp2/ConsoleApp2/Module1.vb
@@ -0,0 +1,23 @@
+﻿Option Explicit On
+Option Strict On
+
+Module Module1
+    Sub Main()
+        Dim num1 As Integer, num2 As String, num3 As String = ""
+
+        Do
+            Console.WriteLine("Please enter an integer : ")
+            num2 = Console.ReadLine
+        Loop Until Integer.TryParse(num2, num1)
+        Console.WriteLine("The Binary number is : {0}", Convert.ToString(num1, 2))
+
+        Do While (num1 > 0)
+            Dim remainder As Integer = num1 Mod 2
+            num3 = remainder.ToString & num3
+            num1 = num1 \ 2
+        Loop
+
+        Console.WriteLine("The Binary number is : {0}", num3)
+        Console.ReadLine()
+    End Sub
+End Module
